56fe2d1d0d8be8c7345195d284fbfeabeb9a1651
Lucas Monteiro <lukasmonteir0@hotmail.com>
2022-02-03 20:51:12 -0300

    chore: add jest

---
+++ jest.config.js
@@ -0,0 +1,12 @@
+/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
+
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  roots: ['<rootDir>/src'],
+  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+  coverageDirectory: 'coverage',
+  transform: {
+    '.+\\.ts$': 'ts-jest'
+  }
+}
