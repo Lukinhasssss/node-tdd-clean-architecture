f40d40c0755cfefbe1d63f853c8374dc12813b4a
Lucas Monteiro <lukasmonteir0@hotmail.com>
2022-02-11 21:40:54 -0300

    fix: update mongodb connection url

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,4 +1,4 @@
 export default {
-  mongoUrl: process.env.MONGO_URL || 'mongodb://localhost:27017/clean-node-api',
-  port: process.env.PORT || 8080
+  mongoUrl: process.env.MONGO_URL ?? 'mongodb://root:password@localhost:27017/clean-node-api?authSource=admin',
+  port: process.env.PORT ?? 8080
 }
